ab447071c610cbf6c20114508edafa594c016e43
Ari <113391785+sariyan-0@users.noreply.github.com>
2026-07-09 13:57:08 +0330

    session #3

--- functions/image_functions.py
+++ functions/image_functions.py
@@ -1,6 +1,6 @@
 ############################### Image Processing Functions ###############################
 from PIL import Image
-
+import random
 
 
 
@@ -12,9 +12,54 @@ def load_image(image_file):
 
 
 def view_image(some_pixels, im_w, im_h):
-
-    new_im = Image.new(mode = "L",
+    if type(some_pixels[0]) == int:
+        new_im = Image.new(mode = "L",
                     size = (im_w, im_h),
                     color = 0)
+    if type(some_pixels[0]) == tuple:
+        new_im = Image.new(mode = "RGB",
+                    size = (im_w, im_h),
+                    color = (0, 0, 0))
     new_im.putdata(some_pixels)
     new_im.show()
+
+def gray2binary(gray_pixels):
+    binary_pixels = []
+    for p in gray_pixels:
+        if p < 128:
+            binary_pixels.append(0)
+        else:
+            binary_pixels.append(255)
+    return binary_pixels
+
+def grb2gray(rgb_pixels):
+    gray_pixels = []
+    for p in rgb_pixels:
+        avg_p = (p[0] + p[1] + p[2]) / 3
+        gray_pixels.append(round(avg_p))
+    return gray_pixels
+
+# def add_noise(pixels):
+#     noisy_pixels = []
+#     for p in pixels:
+#         noise = random.randint(-150, 150)
+#         new_p = p + noise
+#         noisy_pixels.append(new_p)
+#     return noisy_pixels
+
+def add_noise(pixels, intensity):
+    noisy_pixels = []
+    if type(pixels[0]) == int:
+        for p in pixels:
+            noise = random.randint(-intensity, intensity)
+            new_p = p + noise
+            noisy_pixels.append(new_p)
+    if type(pixels[0]) == tuple:
+        for p in pixels:
+            noisy_r = p[0] + random.randint(-intensity, intensity)
+            noisy_g = p[1] + random.randint(-intensity, intensity)
+            noisy_b = p[2] + random.randint(-intensity, intensity)
+
+            noisy_pixels.append((noisy_r, noisy_g, noisy_b))
+
+    return noisy_pixels
--- modules/module_pillow.py
+++ modules/module_pillow.py
@@ -7,79 +7,25 @@ import random
 from PIL import Image
 
 from functions.image_functions import *
-
-w, h,pixs = load_image("images/gray_scale.png")
-
-
-
-im = Image.open("images/gray_scale.png")
-# im.show()
-image_size = im.size    #   (768, 512)
-# print(image_size)       # (width, height)
-#
-pixels = list(im.getdata())
-# print(pixels[:20])
-# ----------------------------------------------------------------------------
-# convert into a binary image
-binary_pixels = []
-for p in pixs:
-    if p < 128:
-        binary_pixels.append(0)
-    else:
-        binary_pixels.append(255)
-# ----------------------------------------------------------------------------
-view_image(binary_pixels, w, h)
-# ----------------------------------------------------------------------------
+# Q1: convert a grayscale image into a binary image
+# w, h,pixs = load_image("images/gray_scale.png")
+# binary_pixels = gray2binary(pixs)
+# view_image(binary_pixels, w, h)
+##############################################################################
 # Q2: convert an RGB image into a grayscale image
-#
-im = Image.open("images/rgb_parrot.png")
-im_size = im.size   # (768, 512)
-print(im_size)     #(width, height)
-pixels = list(im.getdata())
-# print(pixels[:20])
-# ----------------------------------------------------------------------------
-gray_pixels = []
-for p in pixels:
-    avg_p = (p[0] + p[1] + p[2]) / 3
-    gray_pixels.append(round(avg_p))
-
-# print(gray_pixels[:20])
-# ----------------------------------------------------------------------------
-new_im = Image.new(mode = "L",
-                     size = im_size,
-                        color = 0)
-# new_im.putdata(gray_pixels)
-# new_im.show()
+# w, h, pixs = load_image("images/rgb_parrot.png")
+# gray_pixels = grb2gray(pixs)
+# view_image(gray_pixels, w, h)
 # ############################################################################
 # Q3: Add some noice to the grayscale image
-# im = Image.open("images/gray_scale.png")
-# image_size = im.size
-# pixels = list(im.getdata())
-# ----------------------------------------------------------------------------
-# noisy_pixels = []
-# for p in pixels:
-#     noise = random.randint(-20, 20)
-#     new_p = p + noise
-#     noisy_pixels.append(new_p)
-# ----------------------------------------------------------------------------
-# new_im = Image.new(mode = "L",
-#                         size = image_size,
-#                         color = 0)
-# new_im.putdata(noisy_pixels)
-# new_im.show()
+# w, h, pixs = load_image("images/gray_scale.png")
+# noisy_pixels = add_noise(pixs, 50)
+# view_image(noisy_pixels, w, h)
 # ############################################################################
 # Q4: Add some noice to an RGB image
-# im = Image.open("images/rgb_parrot.png")
-# image_size = im.size
-# pixels = list(im.getdata())
-# ----------------------------------------------------------------------------
-# noisy_pixels = []
-# for p in pixels:
-#     noisy_r = p[0] + random.randint(-150, 150)
-#     noisy_g = p[1] + random.randint(-150, 150)
-#     noisy_b = p[2] + random.randint(-150, 150)
-
-#     noisy_pixels.append((noisy_r, noisy_g, noisy_b))
+# w, h, pixs = load_image("images/rgb_parrot.png")
+# noisy_pixels = add_noise(pixs, 100)
+# view_image(noisy_pixels, w, h)
 # ----------------------------------------------------------------------------
 # new_im = Image.new(mode = "RGB", size = image_size, color=(0,0,0))
 # new_im.putdata(noisy_pixels)
@@ -120,3 +66,79 @@ new_im = Image.new(mode = "L",
 #                         color = 0)
 # new_im.putdata(new_pixels)
 # new_im.show()
+
+# ############################################################################
+# Q7: Detect red and green appls
+w, h, pixs = load_image("images/apples/test_2.jpg")
+# def apple_color(file):
+#     w, h, pixs = load_image(file)
+#     red_total = 0
+#     green_total = 0
+
+#     for p in pixs:
+#         red_total += p[0]
+#         green_total += p[1]
+
+#     if red_total > green_total:
+#         print("mostly red")
+#     else:
+#         print("mostly green")
+
+# apple_color("images/apples/test_2.jpg")
+# ----------------------------------------------------------------------------
+# average R , G , B
+# def average_rgb(file):
+#     w, h, pixs = load_image(file)
+#     total_pixels = w * h
+#     sum_r = 0
+#     sum_g = 0
+#     sum_b = 0
+
+#     for p in pixs:
+#         sum_r += p[0]
+#         sum_g += p[1]
+#         sum_b += p[2]
+
+#     avg_r = round(sum_r / total_pixels, 1)
+#     avg_g = round(sum_g / total_pixels, 1)
+#     avg_b = round(sum_b / total_pixels, 1)
+
+#     print(avg_r, avg_g, avg_b)
+
+
+# average_rgb("images/apples/test_2.jpg")
+# ----------------------------------------------------------------------------
+# gray_pixs = grb2gray(pixs)
+# avg_gray = round(sum(gray_pixs) / (w * h), 1)
+# print(avg_gray)
+
+# if avg_gray < 100:
+#     print("mostly green")
+# else:
+#     print("mostly red")
+
+# ############################################################################
+# Q8: Check indivial apples (read and green)
+w, h, pixs = load_image("images/apples/apple.jpg")
+
+
+bg_less_pixels = []
+for p in pixs:
+    if p[0] < 250 and p[1] < 250 and p[2] < 250:
+        bg_less_pixels.append(p)
+
+total_pixels = len(bg_less_pixels)
+sum_r = 0
+sum_g = 0
+sum_b = 0
+
+for p in bg_less_pixels:
+    sum_r += p[0]
+    sum_g += p[1]
+    sum_b += p[2]
+
+avg_r = round(sum_r / total_pixels, 1)
+avg_g = round(sum_g / total_pixels, 1)
+avg_b = round(sum_b / total_pixels, 1)
+
+print(avg_r, avg_g, avg_b)
